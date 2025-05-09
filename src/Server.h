8301503927d5a1ba7b85874aa508156b5abd17d6
Yael <yael.doron1@live.biu.ac.il>
2025-05-09 16:34:49 +0300

    Update Server

--- src/Server.h
+++ src/Server.h
@@ -2,7 +2,12 @@
 
 class Server {
     int port;
+    int serverSocket;
+    BloomFilter filter;
+    UrlStore store;
+
 public:
-    Server(int port); // constructor
+    Server(int port, int filterSize, const std::vector<int>& seeds); // constructor
     int start();
+    void handleClient(int client_sock);
 };
