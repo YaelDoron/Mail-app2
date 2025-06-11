daa72813270492f0ab35df6662041b9c855c3a8a
vikiz3003 <vikiz3003@gmail.com>
2025-06-11 20:14:23 +0300

    Fix: remove duplicate Routes import from App.js to resolve render error

--- src/react_client/src/App.js
+++ src/react_client/src/App.js
@@ -1,21 +1,18 @@
-
+import { Routes, Route, Navigate } from "react-router-dom";
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MailPage from "./pages/MailPage";
-import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage"
 
 function App() {
   return (
-    <Router>
       <Routes>
         <Route path="/mailpage" element={<MailPage />} />
         <Route path="/mailpage/:id" element={<MailPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/" element={<Navigate to="/login" />} />
       </Routes>
-    </Router>
   );
 }
 
