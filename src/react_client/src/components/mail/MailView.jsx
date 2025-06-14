9aa69d5fd9f661a894b26d6e4922b6fa2c6b1646
Yael <yael.doron1@live.biu.ac.il>
2025-06-14 23:44:25 +0300

    Fix single mail view [MA-144]

--- src/react_client/src/components/mail/MailView.jsx
+++ src/react_client/src/components/mail/MailView.jsx
@@ -1,25 +1,68 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import MailHeader from "./MailHeader";
 import MailBody from "./MailBody";
-import { toggleStarMail } from "../../services/mailsService";
+import { getUserById } from "../../services/api";
+import { toggleStarMail, toggleSpamMail, deleteMail } from "../../services/mailsService";
+import { getUserIdFromToken } from "../../services/authService";
+
 
 const MailView = ({ mail: initialMail }) => {
   const [mail, setMail] = useState(initialMail);
+  const [sender, setSender] = useState(null);
+  const [receiver, setReceiver] = useState(null);
+  const navigate = useNavigate();
+  
+
+  useEffect(() => {
+  if (mail?.from) {
+    getUserById(mail.from).then(setSender).catch(console.error);
+  }
+  const currentUserId = getUserIdFromToken();
+  const isSent = mail.from === currentUserId;
+  if (isSent && mail.to?.[0]) {
+    getUserById(mail.to[0]).then(setReceiver).catch(console.error);
+  }
+  }, [mail]);
 
   const handleToggleStar = async (id) => {
     try {
       const updated = await toggleStarMail(id);
-      setMail(updated); // ← מעדכן את כל המייל לפי מה שהשרת החזיר
+      setMail(updated);
     } catch (err) {
-      console.error("שגיאה בסימון כוכב", err);
+      console.error("error in marking as starred", err);
     }
   };
+
+  const handleMarkSpam = async (id) => {
+    try {
+      await toggleSpamMail(id);
+    } catch (err) {
+      console.error("Error marking mail as spam", err);
+    }
+  };
+
+  const handleMarkDelete = async (id) => {
+    try {
+      await deleteMail(id);
+      navigate(-1);
+    } catch (err) {
+      console.error("Error marking mail as trash", err);
+    }
+  };
+
 
   return (
     <div className="flex-grow-1 p-4" style={{ minHeight: "100vh", backgroundColor: "#f8f9fa" }}>
       <div className="bg-white border rounded shadow-sm p-4"
            style={{ maxWidth: "1000px", margin: "0 auto", minHeight: "calc(100vh - 40px)" }}>
-        <MailHeader mail={mail} onToggleStar={handleToggleStar} />
+        <MailHeader
+          mail={mail}
+          onToggleStar={handleToggleStar}
+          onMarkSpam={handleMarkSpam}
+          onDelete={handleMarkDelete}
+          sender={sender}
+        />
         <hr />
         <MailBody content={mail.content} />
       </div>
