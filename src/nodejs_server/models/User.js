54d868c602f1c5b47476c6e9cf56518ed9d92d82
Yael <yael.doron1@live.biu.ac.il>
2025-06-29 15:23:33 +0300

    Add mongo to decker compose

--- src/nodejs_server/models/User.js
+++ src/nodejs_server/models/User.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
   lastName:  { type: String, required: true },
   birthDate: { type: Date, required: true },
-  gender:    { type: String, enum: ['male', 'female', 'other'], required: true },
+  gender:    { type: String, enum: ['Male', 'Female', 'Other','Prefer not to say'], required: true },
   email:     { type: String, required: true, unique: true },
   password:  { type: String, required: true },
   profilePicture: { type: String, default: null }
